e6e08b1ace805943481c06e40985207cfe99f3f3
yinsheng.wang <yinsheng.wang@wwwarehouse.com>
2018-04-24 10:28:15 +0800

    fdfd

---
+++ src/main/java/com/example/testdome/ThreadExccute.java
@@ -0,0 +1,25 @@
+package com.example.testdome;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * Created by yinsheng.wang on 2018/4/24.
+ */
+public class ThreadExccute {
+    private static ExecutorService instance = null;
+
+    private ThreadExccute() {
+    }
+
+    public static ExecutorService getInstance() {
+        if (instance == null) {
+            synchronized (ExecutorService.class) {
+                if (instance == null) {
+                    instance = Executors.newFixedThreadPool(5);
+                }
+            }
+        }
+        return instance;
+    }
+}
---
+++ src/main/java/com/example/testdome/ThreadPoolTest.java
@@ -0,0 +1,76 @@
+package com.example.testdome;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * Created by yinsheng.wang on 2018/4/24.
+ */
+public class ThreadPoolTest {
+    static final ExecutorService executorService = Executors.newFixedThreadPool(5);
+
+    public static void main(String[] args) {
+       /* test1();
+        test2();*/
+
+        for (int i = 0; i < 100; i++) {
+            ThreadExccute.getInstance().execute(new Runnable() {
+                @Override
+                public void run() {
+                    System.out.println(Thread.currentThread().getId() + ">>>>>>>>>>>>>");
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+        }
+        for (int i = 0; i < 100; i++) {
+            ThreadExccute.getInstance().execute(new Runnable() {
+                @Override
+                public void run() {
+                    System.out.println(Thread.currentThread().getId() + "******************");
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+        }
+
+    }
+
+    /*public static void test1() {
+        for (int i = 0; i < 100; i++) {
+            executorService.execute(new Runnable() {
+                @Override
+                public void run() {
+                    System.out.println(Thread.currentThread().getId() + ">>>>>>>>>>>>>");
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+        }
+    }
+
+    public static void test2() {
+        for (int i = 0; i < 100; i++) {
+            ThreadExccute.getInstance().execute(new Runnable() {
+                @Override
+                public void run() {
+                    System.out.println(Thread.currentThread().getId() + "******************");
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+        }
+    }*/
+}
